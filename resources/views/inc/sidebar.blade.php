07d7ee5c6f9f842fa5156a417cb16f91a08fd095
dzalev33 <dzalev33@gmail.com>
2018-05-17 21:50:07 +0200

    routing

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,5 @@
+@section('sidebar')
+    <div class="sidebar">
+        <h3>this is the sidebar</h3>
+        @show
+    </div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE Html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Acme</title>
+</head>
+<body>
+@yield('content')
+
+@include('inc.sidebar')
+
+</body>
+</html>
